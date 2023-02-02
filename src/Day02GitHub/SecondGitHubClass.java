8a0b8bb74bffa431ace89cd7a53ee1dad7e5de86
nesli91 <neslihan_simsar@hotmail.com>
2023-02-02 18:24:29 -0500

    new changes done second class

--- src/Day02GitHub/SecondGitHubClass.java
+++ src/Day02GitHub/SecondGitHubClass.java
@@ -3,6 +3,7 @@ package Day02GitHub;
 public class SecondGitHubClass {
     public static void main(String[] args) {
         System.out.println("I m learning");
+        System.out.println("i m creating");
         System.out.println("2=2");
     }
 }
